3bf78c64c2dbd7cbd342f7c201848dfdc94a0b7d
Diego Jardim <diego.jardim@poatek.com>
2021-04-21 22:19:40 -0300

    Refactoring project files and directories

---
+++ TP2/src/main.py
@@ -0,0 +1,73 @@
+import matplotlib.pyplot as plt
+import pathlib as pl
+from skimage.io import imread, imsave
+import tqdm
+from utils import plot_utils, block_matching
+
+if __name__ == "__main__":
+    is_plot_images = False
+    save_images = True
+    max_disp_steps = 50  # maximum disparity to consider
+    # window_size = 10  # size of the window to consider around the scan line point
+    # penalty = 50
+
+    window_sizes = [1, 2, 4, 8, 16, 32]
+    penalties = [2, 4, 8, 16, 32, 64, 128, 256]
+
+    teddy_images_dir = "../data/saved_images/teddy"
+    cones_images_dir = "../data/saved_images/cones"
+    pl.Path(teddy_images_dir).mkdir(exist_ok=True, parents=True)
+    pl.Path(cones_images_dir).mkdir(exist_ok=True, parents=True)
+
+    left_img_teddy_path = "../data/Teddy/teddy-png-2/im2.png"
+    right_img_teddy_path = "../data/Teddy/teddy-png-2/im6.png"
+
+    img_teddy_left = imread(left_img_teddy_path)
+    img_teddy_right = imread(right_img_teddy_path)
+
+    for window_size in tqdm.tqdm(window_sizes):
+        for idx, penalty in enumerate(penalties):
+            teddy_disp_map = block_matching.get_min_disparity_ssd(l_img=img_teddy_left,
+                                                                  r_img=img_teddy_right,
+                                                                  d_steps=max_disp_steps,
+                                                                  w_size=window_size)
+
+            teddy_disp_map_pen = block_matching.get_min_disparity_ssd(l_img=img_teddy_left,
+                                                                      r_img=img_teddy_right,
+                                                                      d_steps=max_disp_steps,
+                                                                      w_size=window_size,
+                                                                      apply_dist=True,
+                                                                      penalty=penalty)
+
+            left_img_cones_path = "../data/Cones/cones-png-2/im2.png"
+            right_img_cones_path = "../data/Cones/cones-png-2/im6.png"
+
+            img_cones_left = imread(left_img_cones_path)
+            img_cones_right = imread(right_img_cones_path)
+            cones_disp_map = block_matching.get_min_disparity_ssd(l_img=img_cones_left,
+                                                                  r_img=img_cones_right,
+                                                                  d_steps=max_disp_steps,
+                                                                  w_size=window_size)
+
+            cones_disp_map_pen = block_matching.get_min_disparity_ssd(l_img=img_cones_left,
+                                                                      r_img=img_cones_right,
+                                                                      d_steps=max_disp_steps,
+                                                                      w_size=window_size,
+                                                                      apply_dist=True,
+                                                                      penalty=penalty)
+
+            if is_plot_images:
+                plot_utils.plot_images(img_teddy_left, img_teddy_right, teddy_disp_map, teddy_disp_map_pen, penalty)
+                plot_utils.plot_images(img_cones_left, img_cones_right, cones_disp_map, cones_disp_map_pen, penalty)
+                plt.show()
+
+            if save_images:
+                saved_images_teddy = f"{teddy_images_dir}/disp_{idx}_w_{window_size}.png"
+                saved_images_teddy_optim = f"{teddy_images_dir}/disp_{idx}_w_{window_size}_pen_{penalty}.png"
+                saved_images_cones = f"{cones_images_dir}/disp_{idx}_w_{window_size}.png"
+                saved_images_cones_optim = f"{cones_images_dir}/disp_{idx}_w_{window_size}_pen_{penalty}.png"
+
+                imsave(saved_images_teddy, teddy_disp_map)
+                imsave(saved_images_teddy_optim, teddy_disp_map_pen)
+                imsave(saved_images_cones, cones_disp_map)
+                imsave(saved_images_cones_optim, cones_disp_map_pen)
---
+++ TP2/src/utils/plot_utils.py
@@ -0,0 +1,27 @@
+from skimage.color import rgb2gray
+import matplotlib.pyplot as plt
+import matplotlib
+
+matplotlib.use('TkAgg')
+
+
+def plot_images(imageL, imageR, disp_map, disp_map_optim, penalty):
+    fig = plt.figure()
+    ax1 = fig.add_subplot(2, 2, 1)
+    ax1.set_title("Left Image")
+    ax2 = fig.add_subplot(2, 2, 2)
+    ax2.set_title("Right Image")
+    ax3 = fig.add_subplot(2, 2, 3)
+    ax3.set_title("SSD")
+    ax4 = fig.add_subplot(2, 2, 4)
+    ax4.set_title(f"SSD with Penalty of {penalty}")
+    if len(imageL.shape) > 2:
+        ax1.imshow(imageL)
+        ax2.imshow(imageR)
+        ax3.imshow(disp_map, cmap='gray')
+        ax4.imshow(disp_map_optim, cmap='gray')
+    else:
+        ax1.imshow(rgb2gray(imageL), cmap='gray')
+        ax2.imshow(rgb2gray(imageR), cmap='gray')
+        ax3.imshow(disp_map, cmap='gray')
+        ax4.imshow(disp_map_optim, cmap='gray')
